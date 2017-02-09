4717a012b7534f9ac7108ebf40a4f81b327f4592
Godefroy <git@skreo.net>
2017-02-09 11:35:44 +0100

    Add events "useradd" and "userdel"

--- server/server.js
+++ server/server.js
@@ -11,25 +11,33 @@ io.on('connection', (socket) => {
     const userId = uuid.v4()
     const user = {
         id: userId,
-        nickname: 'User' + userId,
+        nickname: 'User' + userId.substr(0, 8),
         socket
     }
+    users.push(user)
 
     // User disconnection
     socket.on('disconnect', () => {
         users.splice(users.indexOf(user), 1)
+        broadcast('userdel', userId)
     })
 
     // Change Nickname
     socket.on('nick', (nickname, reply) => {
         reply = reply || emptyReply
-        if (typeof (nickname) !== 'string') {
+        if (!/[a-z0-9_-]{3,30}/i.test(nickname)) {
             reply({
                 error: "Nickname missing"
             })
             return
         }
-        user.nickname = nickname.substr(0, 30)
+        user.nickname = nickname
+
+        broadcast('useradd', {
+            id: user.id,
+            nickname
+        })
+
         reply()
     })
 
@@ -50,7 +58,11 @@ io.on('connection', (socket) => {
     })
 
     // Broadcast new Users List
-    broadcast('userslist', users.map((user) => {
+    broadcast('useradd', {
+        id: user.id,
+        nickname: user.nickname
+    }, user.id)
+    socket.emit('userslist', users.map((user) => {
         return {
             id: user.id,
             nickname: user.nickname
@@ -59,8 +71,10 @@ io.on('connection', (socket) => {
 })
 
 
-const broadcast = (eventName, data) => {
+const broadcast = (eventName, data, excludeIds) => {
+    excludeIds = excludeIds || []
     users.forEach((user) => {
+        if (excludeIds.indexOf(user.id) !== -1) return
         user.socket.emit(eventName, data)
     })
 }
